2544be9d55c9834e5260ae1371dfddf258af3084
nick momotenko <nickmomotenkoforweb@gmail.com>
2021-06-27 21:57:15 +0300

    add useData hook

--- src/components/EventBar/index.js
+++ src/components/EventBar/index.js
@@ -1,20 +1,12 @@
 import React, { useState } from "react";
 
 import { uuid } from "uuidv4";
-
 import classNames from "classnames";
+
+import { useData } from "../../hooks/useData";
 
 import Card from "../../UI/Card";
 import Title from "../../UI/Title";
-
-import ggBetIcon from "../../assets/partners/gg-bet.png";
-
-import teamIcon1 from "../../assets/teams/1.png";
-import teamIcon2 from "../../assets/teams/2.png";
-import teamIcon3 from "../../assets/teams/3.png";
-import teamIcon4 from "../../assets/teams/4.png";
-
-import battlesGroupIcon from "../../assets/groups/1.png";
 
 import "./EventBar.scss";
 
@@ -23,206 +15,10 @@ const options = [
   { id: uuid(), name: "Battles", status: "battles" },
 ];
 
-const mathes = [
-  {
-    id: uuid(),
-    matchName: "Asia Minor: Play-Off",
-    matchStatus: "live",
-    teams: [
-      {
-        id: uuid(),
-        name: "Albert Warren",
-        icon: teamIcon1,
-        coef: 3.22,
-      },
-      {
-        id: uuid(),
-        name: "Gloria Henry",
-        icon: teamIcon2,
-        coef: 53.22,
-      },
-    ],
-    systemOfСonducting: "Bo 3",
-    partners: [{ id: uuid(), partnerName: "GGBET", partnerIcon: ggBetIcon }],
-  },
-  {
-    id: uuid(),
-    matchName: "Asia Minor: Play-Off",
-    matchStatus: "upcoming",
-    matchTime: {
-      time: "19:00",
-      date: "29 октября",
-    },
-    teams: [
-      {
-        id: uuid(),
-        name: "Regina Watson",
-        icon: teamIcon3,
-        coef: 3.22,
-      },
-      {
-        id: uuid(),
-        name: "Kathryn Jones",
-        icon: teamIcon4,
-        coef: 53.22,
-      },
-    ],
-    systemOfСonducting: "Bo 3",
-    partners: [{ id: uuid(), partnerName: "GGBET", partnerIcon: ggBetIcon }],
-  },
-  {
-    id: uuid(),
-    matchName: "Asia Minor: Play-Off",
-    matchStatus: "live",
-    teams: [
-      {
-        id: uuid(),
-        name: "Albert Warren",
-        icon: teamIcon1,
-        coef: 3.22,
-      },
-      {
-        id: uuid(),
-        name: "Gloria Henry",
-        icon: teamIcon2,
-        coef: 53.22,
-      },
-    ],
-    systemOfСonducting: "Bo 3",
-    partners: [{ id: uuid(), partnerName: "GGBET", partnerIcon: ggBetIcon }],
-  },
-  {
-    id: uuid(),
-    matchName: "Asia Minor: Play-Off",
-    matchStatus: "finished",
-    matchTime: {
-      time: "19:00",
-      date: "29 октября",
-    },
-    teams: [
-      {
-        id: uuid(),
-        name: "Serenity Alexander",
-        icon: teamIcon3,
-        coef: 3.22,
-        maps: 2,
-      },
-      {
-        id: uuid(),
-        name: "Priscilla Lane",
-        icon: teamIcon4,
-        coef: 53.22,
-        maps: 1,
-      },
-    ],
-    gameScore: [
-      {
-        map: "Mirage",
-        score: "1:0",
-      },
-      {
-        map: "Dust2",
-        score: "0:1",
-      },
-      {
-        map: "Nuke",
-        score: "1:0",
-      },
-    ],
-    systemOfСonducting: "Bo 3",
-    partners: [{ id: uuid(), partnerName: "GGBET", partnerIcon: ggBetIcon }],
-  },
-];
-
-const battles = [
-  {
-    id: uuid(),
-    groupName: "Group Stage",
-    groupStatus: "live",
-    groupIcon: battlesGroupIcon,
-    groups: [
-      {
-        id: uuid(),
-        name: "Group A",
-      },
-      {
-        id: uuid(),
-        name: "Group B",
-      },
-    ],
-    map: "Erangel",
-  },
-  {
-    id: uuid(),
-    groupName: "Group Stage",
-    groupStatus: "upcoming",
-    groupIcon: battlesGroupIcon,
-    groupTime: {
-      time: "19:00",
-      date: "March 8",
-    },
-    groups: [
-      {
-        id: uuid(),
-        name: "Group A",
-      },
-      {
-        id: uuid(),
-        name: "Group B",
-      },
-    ],
-    map: "Erangel",
-  },
-  {
-    id: uuid(),
-    groupName: "Group Stage",
-    groupStatus: "upcoming",
-    groupIcon: battlesGroupIcon,
-    groupTime: {
-      time: "19:00",
-      date: "March 8",
-    },
-    groups: [
-      {
-        id: uuid(),
-        name: "Group A",
-      },
-      {
-        id: uuid(),
-        name: "Group B",
-      },
-    ],
-    map: "Erangel",
-  },
-  {
-    id: uuid(),
-    groupName: "Group Stage",
-    groupStatus: "finished",
-    groupIcon: battlesGroupIcon,
-    groupTime: {
-      time: "19:00",
-      date: "March 8",
-    },
-    groups: [
-      {
-        id: uuid(),
-        name: "Group A",
-      },
-      {
-        id: uuid(),
-        name: "Group B",
-      },
-    ],
-    winner: {
-      name: "Natus Vincere",
-      group: "A",
-      pts: "233",
-    },
-  },
-];
-
 const EventBar = () => {
   const [activeOption, setActiveOption] = useState(options[1]);
+
+  const { mathes, battles } = useData();
 
   const changeActiveOption = (item) => {
     setActiveOption(item);
